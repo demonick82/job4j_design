8e49f3de5aa6bbeee222df8a9ce788ba9b591894
demonick82 <demonick82@gmail.com>
2021-05-14 00:55:54 +0300

    4.0. File [#514613]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("C:\\projects\\job4j_design");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        if (file.exists()) {
+            for (File subfile : file.listFiles()) {
+                System.out.printf("Имя файла %s размер %s%n", subfile, subfile.length());
+            }
+        }
+    }
+}
