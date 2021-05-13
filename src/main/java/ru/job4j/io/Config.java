79d69cad228f29dc3bb780ae3773b3a9300783d0
demonick82 <demonick82@gmail.com>
2021-05-14 00:32:10 +0300

    1

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -1,9 +1,7 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
