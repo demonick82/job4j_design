54de22cb77687e167229777bec1175e601b5d808
demonick82 <demonick82@gmail.com>
2021-05-14 00:25:49 +0300

    3.0. Тестирование IO [#514608]

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -1,11 +1,11 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -23,10 +23,9 @@ public class AnalysisTest {
                 builder.append(reader.readLine());
                 builder.append(System.lineSeparator());
             }
-        }  catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        ;
         assertThat(builder.toString(), is("10:57:01;10:59:01\r\n11:01:02;11:02:02\r\n"));
     }
 
@@ -45,8 +44,35 @@ public class AnalysisTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ;
         assertThat(builder.toString(), is("10:57:01;10:59:01\r\n11:01:02;11:02:02\r\n"));
     }
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenStopServerWithTmp() throws IOException {
+        File source = folder.newFile("sourse.log");
+        File target = folder.newFile("target.log");
+        try (PrintWriter writer = new PrintWriter(source)) {
+            writer.println("200 10:56:01");
+            writer.println("500 10:57:01");
+            writer.println("400 10:58:01");
+            writer.println("200 10:59:01");
+            writer.println("500 11:01:02");
+            writer.println("200 11:02:02");
+        }
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.toString(), target.toString());
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            while (reader.ready()) {
+                builder.append(reader.readLine());
+                builder.append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(builder.toString(), is("10:57:01;10:59:01\r\n11:01:02;11:02:02\r\n"));
+    }
 }
