16b6c1ec4b6d70ad164247d938f4cdce4f1efa92
demonick82 <demonick82@gmail.com>
2021-05-17 00:26:00 +0300

    4.2. Поиск дубликатов [#514615] (Исправление)

--- src/main/java/ru/job4j/io/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -4,16 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class DuplicatesFinder {
 
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor visitor = new DuplicatesVisitor();
-        DuplicatesFinder finder = new DuplicatesFinder();
         if (args.length == 0) {
             throw new IllegalArgumentException("Не заданы аргументы коммандной строки");
         }
@@ -22,19 +17,8 @@ public class DuplicatesFinder {
             throw new IllegalArgumentException("Выбранная директория не существует");
         }
         Files.walkFileTree(Path.of(start.toString()), visitor);
-        for (FileProperty fileProperty : finder.duplicateFile(visitor.getPaths())) {
-            System.out.printf("Найден дубликат файла %s", fileProperty.getName());
+        for (FileProperty fileName : visitor.getPaths()) {
+            System.out.printf("Найдены дубликаты файла %s", fileName.getName());
         }
-    }
-
-    public List<FileProperty> duplicateFile(List<FileProperty> list) {
-        Set<FileProperty> compareSet = new HashSet<>();
-        List<FileProperty> rsl = new ArrayList<>();
-        for (FileProperty property : list) {
-            if (!compareSet.add(property)) {
-                rsl.add(property);
-            }
-        }
-        return rsl;
     }
 }
--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -8,16 +8,23 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     private List<FileProperty> list = new ArrayList<>();
+    private Set<FileProperty> set = new HashSet<>();
+
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         String fileName = file.getFileName().toString();
         long fileSize = Files.size(file);
-        list.add(new FileProperty(fileSize, fileName));
+        FileProperty property = new FileProperty(fileSize, fileName);
+        if (!set.add(property)) {
+            list.add(property);
+        }
         return super.visitFile(file, attrs);
     }
 
