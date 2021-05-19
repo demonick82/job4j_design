1e38f642a85c8e83e9bd2b83cac03e198d6a535d
demonick82 <demonick82@gmail.com>
2021-05-19 23:07:55 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,8 +6,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
+    private static Pattern any = Pattern.compile("=\\w*\\s");
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -18,11 +22,20 @@ public class EchoServer {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("=Bye ")) {
+                        Matcher m = any.matcher(str);
+                        if (str.contains("=Hello ")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Hello\r\n\r\n".getBytes());
+                        } else if (str.contains("=Exit ")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Server stop\r\n\r\n".getBytes());
                             return;
+                        } else if (m.find()) {
+                            String answer = (m.group()).substring(1).trim();
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write((answer + System.lineSeparator()).getBytes());
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
                 }
             }
         }
