3b00e1b90bfb9dcda3bec8ab20dde1705798680a
demonick82 <demonick82@gmail.com>
2021-06-24 09:00:41 +0300

    SQL Select by company [#2760 #170913] (Исправления)

--- sql_scripts/sqlCompany/selectPersons.sql
+++ sql_scripts/sqlCompany/selectPersons.sql
@@ -1,6 +1,4 @@
-select p.name from person p where company_id is null AND id=5;
-select p.name, c.name from person p join company c on p.company_id = c.id;
-
+select p.id, p.name, c.name from person p left join company c on p.company_id = c.id and c.id != 5;
 select  c.name, count(c.id)
 from company c join person p on c.id = p.company_id
 group by c.name, c.id
