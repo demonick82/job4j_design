6b430d7748bc093726bc212c9871a2ef13562606
demonick82 <demonick82@gmail.com>
2021-07-12 22:58:47 +0300

    0. Принцип открытости закрытости [#4914] (3)

---
+++ src/main/java/ru/job4j/ood/ocp/AreaCalculator.java
@@ -0,0 +1,13 @@
+package ru.job4j.ood.ocp;
+
+public class AreaCalculator {
+    public Double square(Shape shape) {
+        double rsl = 0.0;
+        if (shape.getClass().equals("Circle")) {
+
+        } else if (shape.getClass().equals("Square")) {
+
+        }
+        return 0.0;
+    }
+}
---
+++ src/main/java/ru/job4j/ood/ocp/Shape.java
@@ -0,0 +1,4 @@
+package ru.job4j.ood.ocp;
+
+public interface Shape {
+}
