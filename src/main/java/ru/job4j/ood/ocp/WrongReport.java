bbc6ffcfb7246e3d0100de45f8130698a5d2e451
demonick82 <demonick82@gmail.com>
2021-07-12 22:46:54 +0300

    0. Принцип открытости закрытости [#4914] (1)

---
+++ src/main/java/ru/job4j/ood/ocp/WrongReport.java
@@ -0,0 +1,20 @@
+package ru.job4j.ood.ocp;
+
+import ru.job4j.ood.srp.Employee;
+
+import java.util.function.Predicate;
+
+public class WrongReport {
+    public String generateReport(Predicate<Employee> filter) {
+        String someText = "";
+        return someText;
+    }
+    public String generateITReport(Predicate<Employee> filter) {
+        String someText = "";
+        return someText;
+    }
+    public String generateHrReport(Predicate<Employee> filter) {
+        String someText = "";
+        return someText;
+    }
+}
