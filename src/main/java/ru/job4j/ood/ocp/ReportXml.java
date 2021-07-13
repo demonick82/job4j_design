38fc57f6a73250e41be6a74ac30623fc2981f3e0
demonick82 <demonick82@gmail.com>
2021-07-14 01:24:34 +0300

    1. Поддержка форматом XML, JSON, HTML в генераторе отчетов. [#851] (Исправлнения)

--- src/main/java/ru/job4j/ood/ocp/ReportXml.java
+++ src/main/java/ru/job4j/ood/ocp/ReportXml.java
@@ -1,7 +1,6 @@
 package ru.job4j.ood.ocp;
 
 import ru.job4j.ood.srp.Employee;
-import ru.job4j.ood.srp.MemStore;
 import ru.job4j.ood.srp.Report;
 import ru.job4j.ood.srp.Store;
 
@@ -9,7 +8,6 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.StringWriter;
-import java.util.Calendar;
 import java.util.function.Predicate;
 
 public class ReportXml implements Report {
@@ -37,19 +35,4 @@ public class ReportXml implements Report {
         }
         return writer.toString();
     }
-
-    public static void main(String[] args) {
-        MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employee worker2 = new Employee("Pavel", now, now, 60);
-        Employee worker = new Employee("Ivan", now, now, 100);
-        Employee worker1 = new Employee("Sergey", now, now, 80);
-        store.add(worker2);
-        store.add(worker);
-        store.add(worker1);
-        ReportXml reportXml = new ReportXml(store);
-        System.out.println(reportXml.generate(employee -> true));
-
-    }
-
 }
