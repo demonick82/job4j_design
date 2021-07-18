a11c48b75a572df28be957ead1fae2fa2f20670c
demonick82 <demonick82@gmail.com>
2021-07-18 18:20:38 +0300

    0. Принцип подстановки Лисков. [#4915] (1)

---
+++ src/main/java/ru/job4j/ood/lsp/Rectangle.java
@@ -0,0 +1,22 @@
+package ru.job4j.ood.lsp;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
