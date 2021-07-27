31ec05984c493f064a7c3dc63ab1d4dfbc45d901
demonick82 <demonick82@gmail.com>
2021-07-27 22:44:47 +0300

    2. Парковка машин [#853 #179891] (2) (исправления)

--- src/test/java/ru/job4j/ood/parking/CarParkingTest.java
+++ src/test/java/ru/job4j/ood/parking/CarParkingTest.java
@@ -3,31 +3,35 @@ package ru.job4j.ood.parking;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.is;
 
 public class CarParkingTest {
 
     @Test
     public void whenCanPark() {
         Car car1 = new PassengerCar();
-        Parking parking = new CarParking(10);
+        Parking parking = new CarParking(10,5);
         assertTrue(parking.canPark(car1));
     }
 
     @Test
     public void whenCanNotPark() {
-        Car car1 = new Truck(5);
-        Car car2 = new Truck(6);
-        Parking parking = new CarParking(10);
+        Car car1 = new PassengerCar();
+        Car car2 = new Truck(3);
+        Car car3 = new Truck(3);
+        Parking parking = new CarParking(1,1);
         parking.canPark(car1);
-        assertFalse(parking.canPark(car2));
+        parking.canPark(car2);
+        assertFalse(parking.canPark(car3));
     }
 
     @Test
-    public void freeSpace() {
-        Car car1 = new Truck(5);
-        Parking parking = new CarParking(10);
+    public void whenTruckParkToCarPoint() {
+        Car car1 = new PassengerCar();
+        Car car2 = new Truck(3);
+        Car car3 = new Truck(3);
+        Parking parking = new CarParking(10,1);
         parking.canPark(car1);
-        assertThat(parking.getParkingFreeSpace(),is(5));
+        parking.canPark(car2);
+        assertTrue(parking.canPark(car3));
     }
 }
